d77307aef35c0bce430033f3313b810fa73bdca1
Uzma Banu <uzmabanu356@gmail.com>
2024-09-12 21:59:03 +0530

    Add file

---
+++ Java/hello.java
@@ -0,0 +1,13 @@
+
+public class hello
+  {
+   public static void main(String args[])
+    {
+      System.out.println("Hello Java");
+      Scanner.sc= new Scanner (System.in);
+      int a= sc.nextInt();
+      int b=sc.nextInt();
+      int sum=a+b;
+      System.out.println("sum=", sum);
+     }
+  }
---
+++ Java/input.java
@@ -0,0 +1,14 @@
+import java .util.Scanner;
+class Input 
+ {
+   public static void main(String[] args);
+   {
+    Scanners sc=new Scanner(System);
+    System.out.println("Enter two number :-");
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     int sum=a+b;
+     System.out.println("sum=");
+    System.out.print(sum);
+
+     
