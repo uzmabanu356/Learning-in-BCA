ab62accb7ab1e39b113214a562cf0e25e8f976a2
Uzma Banu <uzmabanu356@gmail.com>
2025-01-10 22:46:56 +0530

    new file

--- Web File/js/file1.js
+++ Web File/js/file1.js
@@ -6,4 +6,5 @@ console.log("Multiplication: " + (num1 * num2)); // 30
 console.log("Division: " + (num1 / num2)); // 3.333...
 console.log("Modulus: " + (num1 % num2)); // 1
 console.log("Exponentiation: " + (num1 ** num2)); // 1000
-
+console.log("Increament: " +(++num1)); // 1000
+console.log("Decreament: " +(--num2)); // 1000
---
+++ Web File/js/file4.js
@@ -0,0 +1,5 @@
+let x=true;
+let y= false;
+let z=true;
+let andResult= x && y;
+console.log("x && y :"+ andResult);
