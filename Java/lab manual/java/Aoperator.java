2a4f239c7ff5bacf5929cb081386b3fc210e1bd0
Uzma Banu <uzmabanu356@gmail.com>
2025-02-03 22:12:39 +0530

    new file

---
+++ Java/lab manual/java/Aoperator.java
@@ -0,0 +1,20 @@
+public class Aoperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        System.out.println("Addition: " + (a + b));           // Adds a and b
+        System.out.println("Subtraction: " + (a - b));        // Subtracts b from a
+        System.out.println("Multiplication: " + (a * b));     // Multiplies a and b
+        System.out.println("Division: " + (a / b));           // Divides a by b
+        System.out.println("Modulus (Remainder): " + (a % b)); // Remainder when a is divided by b
+    }
+}
+/*
+Output:-
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2
+Modulus (Remainder): 0
+             */
